67325b0f06a7260798339756054cb652a1185a16
Yuri Duarte <yuriduartedev@gmail.com>
2020-08-12 20:22:46 -0300

    Cria rotas autenticadas e logout

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,12 +22,14 @@ interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -39,6 +41,10 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 200);
     }
 
     loadStorageData();
@@ -67,7 +73,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
